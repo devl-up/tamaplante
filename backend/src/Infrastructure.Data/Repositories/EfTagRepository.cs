1e1594895a95f2bc208f0223b2973ca05cdcc954
BusschaertTanguy <tanguy.busschaert.95@gmail.com>
2024-10-18 09:29:53 +0200

    #20 Catalog - Delete Tag

--- backend/src/Infrastructure.Data/Repositories/EfTagRepository.cs
+++ backend/src/Infrastructure.Data/Repositories/EfTagRepository.cs
@@ -1,13 +1,26 @@
 ﻿using Catalog.Domain.Tags.Entities;
 using Catalog.Domain.Tags.Repositories;
 using Infrastructure.Data.Contexts;
+using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Data.Repositories;
 
 internal sealed class EfTagRepository(AppDbContext context) : ITagRepository
 {
+    public Task<List<Tag>> GetTagsAsync(List<Guid> ids)
+    {
+        return context.Set<Tag>()
+            .Where(t => ids.Contains(t.Id))
+            .ToListAsync();
+    }
+
     public Task AddAsync(Tag tag)
     {
         return context.Set<Tag>().AddAsync(tag).AsTask();
+    }
+
+    public void Delete(List<Tag> tags)
+    {
+        context.Set<Tag>().RemoveRange(tags);
     }
 }
---
+++ backend/test/Tamaplante.Tests/Catalog/Application/Tags/Commands/DeleteTagsTests.cs
@@ -0,0 +1,44 @@
+﻿using Catalog.Application.Tags.Commands;
+using Catalog.Domain.Tags.Entities;
+using Catalog.Domain.Tags.Repositories;
+using Common.Application.Commands;
+using FluentAssertions;
+using Moq;
+
+namespace Tamaplante.Tests.Catalog.Application.Tags.Commands;
+
+public sealed class DeleteTagsTests
+{
+    private readonly DeleteTags.Handler _sut;
+    private readonly Mock<ITagRepository> _tagRepositoryMock;
+
+    public DeleteTagsTests()
+    {
+        var unitOfWorkMock = new Mock<IUnitOfWork>();
+        _tagRepositoryMock = new();
+        _sut = new(unitOfWorkMock.Object, _tagRepositoryMock.Object);
+    }
+
+    [Fact]
+    public async Task Handle_Should_Fail_When_ProductsCountDoNotMatch()
+    {
+        // Arrange
+        var command = new DeleteTags.Command([Guid.NewGuid(), Guid.NewGuid(), Guid.NewGuid(), Guid.NewGuid()]);
+
+        var tags = new List<Tag>
+        {
+            new() { Id = command.TagIds[0], Name = "Name 1" },
+            new() { Id = command.TagIds[1], Name = "Name 2" },
+            new() { Id = command.TagIds[2], Name = "Name 3" }
+        };
+
+        _tagRepositoryMock.Setup(r => r.GetTagsAsync(command.TagIds)).ReturnsAsync(() => tags);
+
+        // Act
+        var result = await _sut.HandleAsync(command);
+
+        // Arrange
+        result.Error.Should().NotBeNull();
+        result.Error?.Description.Should().Be("delete-tags-count");
+    }
+}
